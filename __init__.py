bea41987860d3f28ee08349bed528ef2790a7740
Matthias Valvekens <matthias.valvekens@gmail.com>
2018-07-15 00:07:21 +0200

    Enabled Dutch language support
     - Added translation strings
     - Language preference is set in accordance with the user
       record in the database at login.
     - Corrected a few typos in the strings in models.py
       in the process.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+from django.utils import translation
+from django.contrib.auth.signals import user_logged_in
 default_app_config = 'webauth.apps.WebAuthConfig'
+
+def set_session_language(**kwargs):
+    user_language = kwargs['user'].lang
+    translation.activate(user_language)
+    kwargs['request'].session[translation.LANGUAGE_SESSION_KEY] = user_language
+
+user_logged_in.connect(set_session_language)
