91dd0f99cee1687c968e189bb3180d4aae927205
Matthias Valvekens <matthias.valvekens@gmail.com>
2018-08-02 10:35:41 +0200

    Several tweaks to language switcher functionality
     - Index redirect now works properly after changing the url to /index/
       instead of /
     - Language switcher now sets user language preference.

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.views import (
     PasswordResetConfirmView, PasswordResetCompleteView
 )
 from django.utils.translation import ugettext_lazy as _ 
+from django.views import i18n
+from django.utils import translation
 
 from webauth.forms import ActivateAccountForm 
 
@@ -17,3 +19,13 @@ class ActivateAccountView(PasswordResetConfirmView):
 class AccountActivatedView(PasswordResetCompleteView):
     template_name = 'registration/account_activated.html'
     title = _('Account activated')
+
+def set_language(request):
+    response = i18n.set_language(request)
+    user = request.user
+    if user.is_authenticated:
+        # we can now grab the (sanitised/verified) lang code
+        # from the session, since the i18n module took care of that
+        user.lang = request.session[translation.LANGUAGE_SESSION_KEY]
+        user.save()
+    return response
