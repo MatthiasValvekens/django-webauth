40a9b7f563d9e5427f0b459a65de740da1a1b1c0
Matthias Valvekens <matthias.valvekens@gmail.com>
2018-09-10 21:08:05 +0200

    Fixed silly bug in auth backend on nonexistent user login attempt

--- backends.py
+++ backends.py
@@ -15,7 +15,7 @@ class EmailOrLegacyUsernameModelBackend(ModelBackend):
         except User.DoesNotExist:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
-            User.set_password(password)
+            User().set_password(password)
             return None
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
