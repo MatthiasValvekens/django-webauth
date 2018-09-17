2a561949009f39614ce0626c45a17e68520990f6
Matthias Valvekens <matthias.valvekens@gmail.com>
2018-09-18 01:19:16 +0200

    Auto-join a number of related fields on request.user
     - member, board_role and member__board_role are now automatically
    joined

--- backends.py
+++ backends.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.backends import ModelBackend
+from django.conf import settings
 from webauth.models import User
 
 class EmailOrLegacyUsernameModelBackend(ModelBackend):
@@ -20,3 +21,13 @@ class EmailOrLegacyUsernameModelBackend(ModelBackend):
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
+
+    def get_user(self, user_id):
+        qs = User.objects
+        if hasattr(settings, 'WEBAUTH_GET_USER_JOINS'):
+            qs = qs.select_related(*settings.WEBAUTH_GET_USER_JOINS)
+        try:
+            user = qs.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        return user if self.user_can_authenticate(user) else None 
