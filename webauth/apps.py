5ec26f5d0ea9ca1f946015442fa86f9e24d75ee8
Matthias Valvekens <matthias.valvekens@gmail.com>
2018-09-26 01:07:30 +0200

    webauth i18n

--- webauth/apps.py
+++ webauth/apps.py
@@ -3,4 +3,4 @@ from django.utils.translation import ugettext_lazy as _
 
 class WebAuthConfig(AppConfig):
     name = 'webauth'
-    verbose_name = _('LUKWeb Authentication')
+    verbose_name = _('WebAuth authentication layer')
