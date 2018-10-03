a4848e8775fef5aec9ee43e4cb26d79971db5c05
Matthias Valvekens <matthias.valvekens@gmail.com>
2018-10-03 21:41:47 +0200

    Oversight in fields.py

--- webauth/fields.py
+++ webauth/fields.py
@@ -8,7 +8,11 @@ class EmailField(BaseEmailField):
     def get_internal_type(self):
         if USE_CITEXT:
             return 'CI' + super().get_internal_type()
+        else:
+            return super().get_internal_type()
 
     def db_type(self, connection):
         if USE_CITEXT and connection.vendor == 'postgresql':
             return 'citext'
+        else:
+            return super().db_type(connection)
