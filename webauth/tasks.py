9fa76b594601ed6115cd47c4599dde4400fa9b6a
Matthias Valvekens <matthias@mvalvekens.be>
2018-11-24 16:03:55 +0100

    Fix email logging string conversion

--- webauth/tasks.py
+++ webauth/tasks.py
@@ -7,15 +7,24 @@ mail_audit_log = logging.getLogger(__name__ + '.send_mail')
 LOGGING_SEPARATOR = '\n>>>>>%s<<<<<\n\n' % ('=' * 24)
 
 
+def msg_to_string(msg):
+    # essentially from django internals
+    msg = msg.message()
+    mbytes = msg.as_bytes()
+    charset = msg.get_charset()
+    output_charset = charset.get_output_charset() if charset else 'utf-8'
+    return mbytes.decode(output_charset)
+
+
 @shared_task
 def send_mail(message):
     message.send()
-    mail_audit_log.info('Dispatched message:\n%s' % str(message))
+    mail_audit_log.info('Dispatched message:\n%s' % msg_to_string(message))
 
 
 @shared_task
 def send_mails(messages):
     with mail.get_connection() as conn:
         conn.send_messages(messages)
-    msgs = LOGGING_SEPARATOR.join(str(m) for m in messages)
+    msgs = LOGGING_SEPARATOR.join(msg_to_string(m) for m in messages)
     mail_audit_log.info('Dispatched messages:\n%s' % msgs)
